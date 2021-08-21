6769e81dfc1f05f05da5cf5a2cea679b78404833
Irina Romjalis <iromjalis@gmail.com>
2021-08-22 00:32:48 +0200

    with the same random color

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,16 @@
 import React  from 'react';
 import PropTypes from 'prop-types';
 import   './Statistics.css';
+let randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16)
 
-const Statistics = ({ title, stats, randomColor }) => {
+const Statistics = ({ title, stats }) => {
   return (
     <div className="StatisticsWrapper">
       <section className="statistics">
         {title && <h2 className="title">{title}</h2>}
           <ul className="stat-list"> 
             {stats.map(({id, label, percentage}) =>
-            (<li className="item" style={{backgroundColor: randomColor}} key={id}>
+            (<li className="item" style={{backgroundColor:randomColor}} key={id}>
     <span className="label">{label}</span>
     <span className="percentage">{percentage} %</span>
             </li>))}
